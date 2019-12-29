594f0611e97a12bd88dd293361961df5db308397
Ayodele Olalekan <longyarnz@gmail.com>
2019-12-29 09:20:22 +0100

    Initialize templates

--- polls_app/views.py
+++ polls_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Question, Choice
 
-# Create your views here.
+# Get questions and diesplay them
+def index(request):
+    return render(request, 'polls/index.html')
