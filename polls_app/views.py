073397f06e0779f906eb1fc04d27e67a727b9acc
Ayodele Olalekan <longyarnz@gmail.com>
2019-12-29 13:54:02 +0100

    Markup detail and results templates

--- polls_app/views.py
+++ polls_app/views.py
@@ -13,7 +13,7 @@ def detail(request, question_id):
         question = Question.objects.get(pk = question_id)
     except Question.DoesNotExist:
         raise Http404('Question does not exist')
-    return render(request, 'polls/results.html', { 'question': question })
+    return render(request, 'polls/detail.html', { 'question': question })
 
 # Get question and display results
 def results(request, question_id):
