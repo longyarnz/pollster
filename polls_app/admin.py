736286065b91a745ac2e35b4140afd738aa5103a
Ayodele Olalekan <longyarnz@gmail.com>
2019-12-29 07:29:16 +0100

    Restructure file system

--- polls_app/admin.py
+++ polls_app/admin.py
@@ -1,15 +1,3 @@
 from django.contrib import admin
-from .models import Question, Choice
 
-
-class ChoiceInline(admin.TabularInline):
-    model = Choice
-    extra = 3
-
-
-class QuestionAdmin(admin.ModelAdmin):
-    fieldsets = [(None, {'fields': ['question_text']}), ('Date Information', {
-        'fields': ['pub_date'], 'classes': ['collapse']}), ]
-
-# admin.site.register(Question)
-# admin.site.register(Choice)
+# Register your models here.
